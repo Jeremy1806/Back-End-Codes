fb62f97f889a6f0b1bd713b274f52693691306ff
Jeremy1806 <76775829+Jeremy1806@users.noreply.github.com>
2022-04-16 23:05:47 +0530

    added routing

--- Simple Web Server/index.js
+++ Simple Web Server/index.js
@@ -3,7 +3,18 @@ const http = require("http");
 const url = require("url");
 
 const server = http.createServer((req, res) => {
-  res.send("Hello i am server");
+  const path = req.url;
+  if (path === "/" || path === "/overview") {
+    res.end("This is the overview");
+  } else if (path === "/items") {
+    res.end("This is the item section");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
+    });
+    res.end("<h1>Page Not Found</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
